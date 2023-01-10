d6629bc48cb901dfe9ed5a4098fc8c49c2e20164
amexabee <amexabee@yahoo.com>
2023-01-10 02:18:55 +0200

    Add movies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Movies from './components/movies';
 
 function App() {
   return (
     <main className="container">
-      <h1>Hello World</h1>
+      <Movies />
     </main>
   );
 }
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,9 +6,48 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
-  handleDelete = (movie) => {};
+  handleDelete = (id) => {
+    const movies = this.state.movies.filter((movie) => movie._id !== id);
+    this.setState({ movies });
+  };
+
   render() {
-    return <h1>whar up!</h1>;
+    if (this.state.movies.length === 0)
+      return <p>There are no movies in the database.</p>;
+    return (
+      <>
+        <p>Showing {this.state.movies.length} movies in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stoke</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie._id)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
   }
 }
 
