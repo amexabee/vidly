de4bad4e6b2718e1df42bc001c2b8e0cb980aea6
amexabee <amexabee@yahoo.com>
2023-02-11 11:07:57 +0200

    Implement navigation to and from movieForm

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const MovieForm = () => {
-  const { id } = useParams();
+  // const { id } = useParams();
   const [navigate, setNavigate] = useState(false);
   if (navigate) return <Navigate to="/" replace={true} />;
 
   return (
     <div>
-      <h1>Movie Form -{id}</h1>
+      <h1>Movie Form</h1>
       <button className="btn btn-primary" onClick={() => setNavigate(true)}>
         Save
       </button>
